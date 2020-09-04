5c90a8824e7406df5501d8bde529393cdf3febbe
Michael Ross <mross.j@gmail.com>
2020-09-04 13:25:51 -0400

    complete exercises
    > completed problems: 1, 2, 3, and 5

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,21 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    
+    # create cache
+    cache = {}
+    # loop through list
+    for i in range(length):
+        # set key
+        w = weights[i]
+        # compare limit and w
+        value = (limit - w)
+        # if value exists in table
+        if value in cache:
+            result = cache[value]
+            # return tuple
+            return (i, result)
+        else:
+            # set key to index
+            cache[w] = i
     return None
